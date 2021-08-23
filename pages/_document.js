a1298798e59ac2d74031121d696d253c8bc4a149
asanoviskhak <asanov.personal@gmail.com>
2021-08-23 22:43:30 +0600

    Fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/Script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Script from 'next/Script'
+import Script from 'next/script'
 import Image from 'next/image'
 export default function Home() {
   return (
